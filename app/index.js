a55f848efc5a4cc964eaf84b778f078cfb6fd123
Eric Craft <eacraft13@gmail.com>
2016-08-31 04:21:22 -0500

    Make it an angular app

--- app/index.js
+++ app/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World! From Eric.');
-});
+app.use(express.static('public'));
 
 app.listen(8080);
